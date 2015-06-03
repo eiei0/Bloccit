00029c90349a7e21d61fd708a5d2655453271a67
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-06-03 14:11:37 -0500

    halfway through assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'question/index'
+
+  get 'question/new'
+
+  get 'question/create'
+
+  get 'question/show'
+
+  get 'question/edit'
+
+  get 'question/update'
+
+  get 'question/destroy'
+
   resources :posts, :advertisement
 
   get 'about' => 'welcome#about'
