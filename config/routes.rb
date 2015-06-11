e1a0bd8ef48c6eed20f97d08874c642fa69fb5dc
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-06-10 23:28:09 -0500

    QuestionsController not complete. Back to square one after digging myself out of a hole with the help of Ben

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'question/index'
-
-  get 'question/new'
-
-  get 'question/create'
-
-  get 'question/show'
-
-  get 'question/edit'
-
-  get 'question/update'
-
-  get 'question/destroy'
-
-  resources :posts, :advertisement
+  resources :posts, :advertisement, :questions
 
   get 'about' => 'welcome#about'
 
