fa0cee16d8324426f263f7c31a6595e070208532
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-07-15 11:19:04 -0500

    fixed migrations issue manually and copied applicaiton.html.erb from assignment-AuthenticationContinued

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   resources :posts, except: [:show]
-=======
   resources :posts, :advertisement, :questions
->>>>>>> assignment-MoreCRUD
-=======
   devise_for :users
   resources :posts
->>>>>>> checkpoint-UserAuthentication
 
   get 'about' => 'welcome#about'
 
