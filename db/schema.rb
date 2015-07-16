de1a320acfcb204f211b500e3398ee9e5c3f2aec
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-07-16 08:52:53 -0500

    issue with rake db:reset for Levi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619033309) do
+ActiveRecord::Schema.define(version: 20150715180814) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150619033309) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,24 @@ require 'faker'
 unique_post = 'This is a unique post'
 unique_comment = 'This is a unique comment'
 
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+    user.skip_confirmation!
+    user.save!
+  end
+  users = User.all
+
 50.times do
- Post.create!(
-   title:  Faker::Lorem.sentence,
-   body:   Faker::Lorem.paragraph
- )
-end
+   Post.create!(
+     user:   users.sample,
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph
+   )
+   end
 posts = Post.all
 
 unless Post.where(title: unique_post).exists?
@@ -19,6 +31,7 @@ Post.create_with(body: Faker::Lorem.paragraph).find_or_create_by(title: unique_p
 
 100.times do
  Comment.create!(
+  # user: users.sample,   # we have not yet associated Users with Comments
    post: posts.sample,
    body: Faker::Lorem.paragraph
  )
@@ -28,6 +41,14 @@ unless Comment.where(body: unique_comment).exists?
   Comment.create!(body: unique_comment, post: posts.sample)
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'youremail.com',
+  password: 'helloworld'
+  )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
