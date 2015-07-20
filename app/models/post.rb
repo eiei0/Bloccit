2aae51a1da2ad0738bab0ff0815f6f3d773b9471
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-07-20 10:46:43 -0500

    fixed issues with app with Ben

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ActiveRecord::Base
     default_scope { order(created_at: :desc) }
     scope :ordered_by_title, -> { reorder(title: :asc) }
     scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+    scope :alexs, -> { where(user_id: 5) }
 end
