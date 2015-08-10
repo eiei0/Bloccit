5004338889ba779088b36cb9ff7fc262b2e83e3f
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-08-09 20:24:22 -0500

    post validations, refactoring, and partials implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,48 +11,41 @@ class PostsController < ApplicationController
     authorize @post
   end
 
-  def create
-    @post = Post.new(post_params)
-    @post.user = current_user
-    authorize @post
-    if @post.save
-      flash[:notice]  = "Post was saved."
-      redirect_to [@topic, @post]
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :new
-    end
-  end
- 
- def create
-      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-     authorize @post
-     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :new
-     end
-   end
-
   def edit
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
       authorize @post
   end
 
+ def create
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.new(post_params)
+     @post.user = current_user
+     @post.topic = @topic
+     authorize @post
+
+     if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
+
   def update
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    authorize @post
-    if @post.update_attributes(post_params)
-      flash[:notice] = "Post was updated"
-      redirect_to [@topic, @post]
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-    end
-  end
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+
+     if @post.update_attributes(post_params)
+       flash[:notice] = "Post was updated."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
 
   private
 
