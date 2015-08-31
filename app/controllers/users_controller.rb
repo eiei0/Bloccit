7a546f05e65ba28c883cf957ac8fd63ffcaa3954
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-08-31 09:31:00 -0500

    TDD errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- before_action :authenticate_user!
+ before_action :authenticate_user!, except: [:show]
 
  def update
    if current_user.update_attributes(user_params)
@@ -16,4 +16,11 @@ class UsersController < ApplicationController
  def user_params
    params.require(:user).permit(:name, :avatar)
  end
+
+ def show
+  @user = User.find(params[:id])
+  @posts = @user.posts
+  @comments = @user.comments
+ end
+
 end
