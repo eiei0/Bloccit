8b7cfc78460ea18b4900eedce86b60fc9526f8c2
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-09-03 08:45:25 -0500

    fixed errors from other checkpoints and edded create comments functionality with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,29 @@
 class CommentsController < ApplicationController
-  
-def destroy
+
+ def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
+    @comment.post = @post
+    @new_comment = Comment.new
+
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment was created."
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
    @post = Post.find(params[:post_id])
    @comment = @post.comments.find(params[:id])
    authorize @comment
@@ -10,12 +33,7 @@ def destroy
    else
      flash[:error] = "Comment couldn't be deleted. Try again."
    end
-
-   respond_to do |format|
-     format.html
-     format.js
-   end
- end
+  end
 
   private
 
