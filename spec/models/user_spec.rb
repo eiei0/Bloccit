90a2de68da99490e9e2ca0531638dd64d8c9d0c4
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-03-16 00:19:26 -0500

    Add specs for #favorited(post)
    Also remove whitespace and indentation issues in the file.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,45 @@
 require 'rails_helper'
 
-describe User do 
+describe User do
 
   include TestFactories
 
+  before do
+    @user1 = create(:user)
+    @post1 = create(:post, user: @user1)
+    create(:comment, user: @user1, post: @post1)
+
+    @user2 = create(:user)
+    @post2 = create(:post, user: @user2)
+    2.times { create(:comment, user: @user2, post: @post2) }
+  end
+
   describe ".top_rated" do
- 
-     before do
-       @user1 = create(:user)
-       post = create(:post, user: @user1)
-       create(:comment, user: @user1, post: post)
- 
-       @user2 = create(:user)
-       post = create(:post, user: @user2)
-       2.times { create(:comment, user: @user2, post: post) }
-     end
- 
-     it "returns users ordered by comments + posts" do
-       expect( User.top_rated ).to eq([@user2, @user1])
-     end
- 
-     it "stores a `posts_count` on user" do
-       users = User.top_rated
-       expect( users.first.posts_count ).to eq(1)
-     end
- 
-     it "stores a `comments_count` on user" do
-       users = User.top_rated
-       expect( users.first.comments_count ).to eq(2)
-     end
-   end
-   
-  describe "#favorited(post)" do
-    xit "returns `nil` if the user has not favorited the post" do 
+    it "returns users ordered by comments + posts" do
+      expect( User.top_rated ).to eq([@user2, @user1])
     end
 
-    xit "returns the appropriate favorite if it exists" do 
+    it "stores a `posts_count` on user" do
+      users = User.top_rated
+      expect( users.first.posts_count ).to eq(1)
+    end
+
+    it "stores a `comments_count` on user" do
+      users = User.top_rated
+      expect( users.first.comments_count ).to eq(2)
+    end
+  end
+
+  describe "#favorited(post)" do
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user1.favorited(@post1)).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = Favorite.new(post_id: @post1.id, user_id: @user1.id)
+      favorite.save!
+
+      expect(@user1.favorited(@post1)).to eq(favorite)
     end
   end
 end
