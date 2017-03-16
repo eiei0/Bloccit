d73c10649e9710a9a27a4bba182bdd85749a8cdc
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-03-16 00:18:47 -0500

    Remove whitespace and fix indentation

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
- 
+
  describe "Sign in flow" do
-   
-   include TestFactories
- 
-   describe "successful" do
-     it "redirects to the topics index" do
+  include TestFactories
+
+  describe "successful" do
+    it "redirects to the topics index" do
       user = authenticated_user
       visit root_path
 
       within '.user-info' do
-       click_link 'Sign In'
+        click_link 'Sign In'
       end
+
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
 
